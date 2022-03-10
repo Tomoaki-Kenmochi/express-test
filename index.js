14743cc2e91acdfb092db61df20d21fe7e86c38a
釼持智昭（Oasis） <toky.ffd@gmail.com>
2022-03-10 20:50:34 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require("path")
+const app = express()
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.get('/', function (req, res) {
+//   console.log(req)
+//   res.send('')
+// });
+
+app.post('/api/v1/quiz', function (req, res) {
+  const answer = req.body.answer;
+  if (answer === "2") {
+    // res.send("正解");
+    res.redirect("/correct.html");
+  } else {
+    // res.send("不正解");
+    res.redirect("/wrong.html");
+  }
+  res.send(answer);
+
+});
+
+app.listen(3000, function(){
+console.log("I am running!")
+});
